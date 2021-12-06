4156e6b63b3d987641a66ab8191e12ecae97fc39
Saurabh Agarwal <himanshu.daksha007@gmail.com>
2021-12-05 21:01:51 -0600

    Code Baseline

---
+++ TestConsoleApplication/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+
+string guid = System.Guid.NewGuid().ToString();
+string testString = "Test File";
+byte[] buffer = Encoding.ASCII.GetBytes(testString);
+File.WriteAllBytes("c:/temp/" + guid + ".txt", buffer);
+
+Console.WriteLine("Hello, World!");
